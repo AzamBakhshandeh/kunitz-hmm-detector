7d8819fd12978eb058620a44972b5f85269e41a0
AzamBakhshandeh <129414208+AzamBakhshandeh@users.noreply.github.com>
2025-05-18 21:22:03 +0200

    Add files via upload

---
+++ scripts/get_seq.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys
+
+def get_ids(idlist):
+    with open(idlist) as f:
+        return f.read().strip().split('\n')
+
+def getseq(pidlist, seqfile):
+    with open(seqfile) as f:
+        s = 0
+        for line in f:
+            if line.startswith('>'):
+                pid = line.split('|')[1]
+                s = 0
+                if pid in pidlist:   # ✅ تغییر اینجا
+                    s = 1
+            if s == 1:
+                print(line.strip())
+
+if __name__ == '__main__':
+    idlist = sys.argv[1]
+    seqfile = sys.argv[2]
+    pidlist = get_ids(idlist)
+    getseq(pidlist, seqfile)
